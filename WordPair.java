88e3bdae64b0669a908d63ad2ec2a6ea8346f7e4
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2025-05-04 03:02:29 +0000

    changed

---
+++ WordPair.java
@@ -0,0 +1,18 @@
+public class WordPair {
+    private String first;
+    private String second;
+    public WordPair (String f, String s ) {
+        first = f; 
+        second = s;
+
+    }
+    public String getFirst () {
+        return first;
+    }
+    public String getSecond () {
+        return second;
+    }
+    public String toString () {
+        return "(\""+first+"\", \""+second+"\")";
+    }
+}
